20befe74b20f8ef7beda8f44499a9a4140590f54
Furkan <ermagfurkan7@gmail.com>
2023-10-23 22:17:40 +0300

    aaaaaa

--- index.js
+++ index.js
@@ -1,8 +1,23 @@
 const express = require('express')
+const fs = require('fs')
 const app = express()
+const path = require("path");
 
-app.use('/video', express.static('videos'))
 
+
+app.get('/video/:video', (req, res) => {
+   const videoPath = path.join(__dirname, "videos", req.params.video);
+   const videoSize = fs.statSync(videoPath).size;
+   
+   const headers = {
+       "Content-Length": videoSize,
+       "Content-Type": "video/mp4",
+   };
+   res.writeHead(200, headers);
+   
+   const videoStream = fs.createReadStream(videoPath);
+   videoStream.pipe(res);
+})
 
 app.get('/', function (req, res) {
    res.send('Ekin <3')
