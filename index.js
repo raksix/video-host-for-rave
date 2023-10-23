449e6b5635afd30fd80f21e831ee0f0de9884607
Furkan <ermagfurkan7@gmail.com>
2023-10-23 22:36:00 +0300

    dasdsadadsa

--- index.js
+++ index.js
@@ -7,32 +7,33 @@ const path = require("path");
 app.get('/video/:video', (req, res) => {
    const videoPath = path.join(__dirname, "videos", req.params.video);
    const videoSize = fs.statSync(videoPath).size;
-   
+
    const CHUNK_SIZE = 10 ** 6; // Örnek olarak 1 MB
-   
+
    const headers = {
-       "Content-Type": "video/mp4",
+      "Content-Length": videoSize,
+      "Content-Type": "video/mp4",
    };
    res.writeHead(200, headers);
 
    const readStream = fs.createReadStream(videoPath, { highWaterMark: CHUNK_SIZE });
 
    readStream.on("data", (chunk) => {
-       if (!res.write(chunk)) {
-           readStream.pause();
-       }
+      if (!res.write(chunk)) {
+         readStream.pause();
+      }
    });
 
    res.on("drain", () => {
-       readStream.resume();
+      readStream.resume();
    });
 
    readStream.on("end", () => {
-       res.end();
+      res.end();
    });
 
    res.on("close", () => {
-       readStream.destroy();
+      readStream.destroy();
    });
 });
 
