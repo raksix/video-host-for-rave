e7aa349ca212b71ab918c34110f72d46210e62d5
Furkan <ermagfurkan7@gmail.com>
2023-10-23 22:29:54 +0300

    dasds

--- index.js
+++ index.js
@@ -4,20 +4,37 @@ const app = express()
 const path = require("path");
 
 
-
 app.get('/video/:video', (req, res) => {
    const videoPath = path.join(__dirname, "videos", req.params.video);
    const videoSize = fs.statSync(videoPath).size;
    
+   const CHUNK_SIZE = 10 ** 6; // Örnek olarak 1 MB
+   
    const headers = {
-       "Content-Length": videoSize,
        "Content-Type": "video/mp4",
    };
    res.writeHead(200, headers);
-   
-   const videoStream = fs.createReadStream(videoPath);
-   videoStream.pipe(res);
-})
+
+   const readStream = fs.createReadStream(videoPath, { highWaterMark: CHUNK_SIZE });
+
+   readStream.on("data", (chunk) => {
+       if (!res.write(chunk)) {
+           readStream.pause();
+       }
+   });
+
+   res.on("drain", () => {
+       readStream.resume();
+   });
+
+   readStream.on("end", () => {
+       res.end();
+   });
+
+   res.on("close", () => {
+       readStream.destroy();
+   });
+});
 
 app.get('/', function (req, res) {
    res.send('Ekin <3')
