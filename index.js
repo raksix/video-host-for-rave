d6fccb49f66c6931341becc431e11a9855bc8f74
Furkan <ermagfurkan7@gmail.com>
2023-10-24 16:46:10 +0300

    dasdas

--- index.js
+++ index.js
@@ -8,31 +8,24 @@ app.get('/video/:video', (req, res) => {
    const videoPath = path.join(__dirname, "videos", req.params.video);
    const videoStat = fs.statSync(videoPath);
    const fileSize = videoStat.size;
-   const videoRange = req.headers.range;
-   if (videoRange) {
-      const parts = videoRange.replace(/bytes=/, "").split("-");
-      const start = parseInt(parts[0], 10);
-      const end = parts[1]
-         ? parseInt(parts[1], 10)
-         : fileSize - 1;
-      const chunksize = (end - start) + 1;
-      const file = fs.createReadStream(videoPath, { start, end });
-      const head = {
-         'Content-Range': `bytes ${start}-${end}/${fileSize}`,
-         'Accept-Ranges': 'bytes',
-         'Content-Length': chunksize,
-         'Content-Type': 'video/mp4',
-      };
-      res.writeHead(206, head);
-      file.pipe(res);
-   } else {
-      const head = {
-         'Content-Length': fileSize,
-         'Content-Type': 'video/mp4',
-      };
-      res.writeHead(200, head);
-      fs.createReadStream(videoPath).pipe(res);
-   }
+   const start = 0
+   const end = 0
+      ? parseInt(0, 10)
+      : fileSize - 1;
+   const chunksize = (end - start) + 1;
+   const file = fs.createReadStream(videoPath, { start, end });
+   const head = {
+      'Content-Range': `bytes ${start}-${end}/${fileSize}`,
+      'Accept-Ranges': 'bytes',
+      'Content-Length': chunksize,
+      'Content-Type': 'video/mp4',
+      'Access-Control-Allow-Credentials': 'true',
+      'Access-Control-Allow-Headers': 'true',
+      
+   };
+   res.writeHead(206, head);
+   file.pipe(res);
+
 });
 
 app.get('/', function (req, res) {
